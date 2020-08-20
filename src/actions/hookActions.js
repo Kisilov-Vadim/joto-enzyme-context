6bf9d74275b21b4a8f9b0b33eedfc15b666a52e3
Vadim Kisilov <vadimkisilov@MacBook-Pro-Vadim.local>
2020-08-20 12:00:18 +0300

    commit

--- src/actions/hookActions.js
+++ src/actions/hookActions.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
 export const getSecretWord = async (setSecretWord) => {
-  const response = await axios.get('http://localhost:3030');
-  setSecretWord(response.data);
+  const response = await axios.get('https://api.datamuse.com/words?ml=ringing+in+the+ears');
+  let randomNum = Math.floor(Math.random() * (response.data.length - 0)) + 0; 
+  setSecretWord(response.data[randomNum].word);
 }
 
 // default export for mocking convenience
